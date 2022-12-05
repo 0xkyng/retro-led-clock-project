81e12af157c9118d571f1dea01476f387e66aaee
Isaac Wanger <teryima127@gmail.com>
2022-12-05 20:33:20 +0100

    improved features

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"github.com/inancgumus/screen"
+)
 
 func main() {
 	type placeholder [5]string
@@ -96,12 +100,47 @@ func main() {
 	// + The underlying type of [5]string and placeholder is the same:
 	//     [5]string
 
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
 	digits := [...]placeholder{
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	fmt.Println(digits[0])
+	screen.Clear()
 
+	for {
+		screen.MoveTopLeft()
+		now := time.Now()
+	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+	clock := [...]placeholder{
+		digits[hour/10], digits[hour%10],
+		colon,
+		digits[min/10], digits[min%10],
+		colon,
+		digits[sec/10], digits[sec%10],
+	}
+
+	for line := range clock[0] {
+		for index, digit := range clock {
+			next := clock[index][line]
+			if digit == colon && sec%2 == 0 {
+				next = "   "
+			}
+			fmt.Print(next,"  ")
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+	time.Sleep(time.Second)
+
+	}
 }
 
 // Digit character       : █
